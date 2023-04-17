0de31320c0ec43b50a4153e57e53fd27c13e7d18
Bautista Renaudo <bautistarenaudo@gmail.com>
2023-04-17 19:04:47 -0300

    final for sure

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,89 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useEffect, useState } from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [newValue, setNewValue] = useState("");
+
+  const [list, setList] = useState(() => {
+    const localValue = localStorage.getItem("ITEMS")
+    if (localValue == null) return []
+
+    return JSON.parse(localValue)
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEMS", JSON.stringify(list))
+  }, [list])
+
+
+
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    {
+      setNewValue("")
+      setList([...list,
+      {
+        text: newValue,
+        id: crypto.randomUUID(),
+        com: false
+      }])
+    }
+  }
+
+  const toggleItem = (id, com) => {
+    console.log(com, id)
+    setList(currentList => {
+      return currentList.map(item => {
+        if (item.id == id) {
+          item.com = !com;
+        }
+
+        return item
+      })
+    })
+  }
+
+  const deleteItem = (id) => {
+    setList(currentList => {
+      return currentList.filter(item => item.id != id)
+    })
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <>
+      <form action="" onSubmit={handleSubmit}>
+        <h1>To Do List</h1>
+        <input
+          type="text"
+          value={newValue}
+          onChange={(e) => { setNewValue(e.target.value) }} />
+        <button type='submit'>+</button>
+      </form>
+
+      <div className='list'>
+        {list.map(item => {
+          return (
+            <li
+              key={crypto.randomUUID()}
+              id={item.id}
+              className={item.com && "completed"}>
+              <div
+                className="text"
+                onClick={(e) => {
+                  toggleItem(
+                    item.id,
+                    //e.target.closest("li").className == "completed")
+                    item.com)
+                }}>{item.text}</div>
+              <div
+                className="delete"
+                onClick={() => { deleteItem(item.id) }}>+</div></li>
+          )
+        })}
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </div>
+    </>
   )
 }
 
